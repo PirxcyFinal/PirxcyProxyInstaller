469a069e52d8be1b63db94e313689bbba8db9771
kam <71592833+PirxcyFinal@users.noreply.github.com>
2024-06-12 14:30:34 +0100

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -1,7 +1,3 @@
-$ErrorActionPreference = "Stop"
-# Enable TLSv1.2 for compatibility with older clients for the current session
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-# Define the URL of the text file
 $url = "https://github.com/PirxcyFinal/PirxcyProxyFinal/raw/main/main.py"
 curl -L -o "main.py" "https://github.com/PirxcyFinal/PirxcyProxyFinal/raw/main/main.py"
