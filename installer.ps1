c754028cd6877560c1e87369ec20e68f05fa1157
kam <71592833+PirxcyFinal@users.noreply.github.com>
2024-06-12 14:35:13 +0100

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -1,1 +1,4 @@
+$ErrorActionPreference = "Stop"
+# Enable TLSv1.2 for compatibility with older clients for current session
+[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -Uri "https://github.com/PirxcyFinal/PirxcyProxyFinal/raw/main/main.py" -OutFile "main.py"
