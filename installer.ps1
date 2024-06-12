c21ccd662894e83627d3ae0e9d22694e8d8b4ce0
kam <71592833+PirxcyFinal@users.noreply.github.com>
2024-06-12 14:25:42 +0100

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "Stop"
-# Enable TLSv1.2 for compatibility with older clients for current session
-[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12
+# Enable TLSv1.2 for compatibility with older clients for the current session
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 # Define the URL of the text file
 $url = "https://github.com/PirxcyFinal/PirxcyProxyFinal/raw/main/main.py"
@@ -10,14 +10,18 @@ $filePath = "main.py"
 
 # Ensure the directory exists, if not create it
 $directory = [System.IO.Path]::GetDirectoryName($filePath)
-if (-not (Test-Path -Path $directory)) {
+if (-not (Test-Path -Path $directory) -and $directory -ne $null) {
     New-Item -ItemType Directory -Path $directory -Force
 }
 
-# Download the content from the URL
-$response = Invoke-WebRequest -Uri $url
+try {
+    # Download the content from the URL
+    $response = Invoke-WebRequest -Uri $url
 
-# Save the content to the text file
-$response.Content | Out-File -FilePath $filePath -Encoding UTF8
+    # Save the content to the text file
+    $response.Content | Out-File -FilePath $filePath -Encoding UTF8
 
-Write-Output "[+] $filePath installed!"
+    Write-Output "[+] $filePath installed successfully!"
+} catch {
+    Write-Error "Failed to download or save the file: $_"
+}
