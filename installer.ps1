e6c14fb71390f5aebc4a292e77ae52ec83d256a6
kam <71592833+PirxcyFinal@users.noreply.github.com>
2024-06-12 14:29:58 +0100

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -4,24 +4,4 @@ $ErrorActionPreference = "Stop"
 
 # Define the URL of the text file
 $url = "https://github.com/PirxcyFinal/PirxcyProxyFinal/raw/main/main.py"
-
-# Define the path where the text file will be saved
-$filePath = "/main.py"
-
-# Ensure the directory exists, if not create it
-$directory = [System.IO.Path]::GetDirectoryName($filePath)
-if (-not (Test-Path -Path $directory) -and $directory -ne $null) {
-    New-Item -ItemType Directory -Path $directory -Force
-}
-
-try {
-    # Download the content from the URL
-    $response = Invoke-WebRequest -Uri $url
-
-    # Save the content to the text file
-    $response.Content | Out-File -FilePath $filePath -Encoding UTF8
-
-    Write-Output "[+] $filePath installed successfully!"
-} catch {
-    Write-Error "Failed to download or save the file: $_"
-}
+curl -L -o "main.py" "https://github.com/PirxcyFinal/PirxcyProxyFinal/raw/main/main.py"
